318c34914864847d578706ca385dccfdada0fa7d
yousufabu <yousuf.abu1996@gmail.com>
2023-03-18 20:59:16 -0400

    addConstructor

---
+++ src/ConstructorPKG/Constructor.java
@@ -0,0 +1,30 @@
+package ConstructorPKG;
+
+public class Constructor {
+	// global variable
+	int     age   = 20;
+	double height =2;
+	String race   ="Asian";
+	String first  ="Abu";
+	String last	  ="Yousuf";
+	
+
+	Constructor(int i){
+		// local variable
+		int myage = age;
+		System.out.println(myage);
+		
+	}
+	Constructor(String s, String q, String d){
+		String myFirst = "Abu";
+		String myLast = "Yousuf";
+		String myRace=  "Asian";
+		System.out.println(myFirst + myLast + myRace);
+		
+	}	
+	
+	
+	
+	
+
+}
---
+++ src/ConstructorPKG/UseConstructor.java
@@ -0,0 +1,14 @@
+package ConstructorPKG;
+
+public class UseConstructor {
+
+	public static void main(String[] args) {
+		 
+		Constructor con = new Constructor("ABU ", "YOUSUF ", " ASIAN");
+		
+		
+		
+		
+	
+	
+}}
